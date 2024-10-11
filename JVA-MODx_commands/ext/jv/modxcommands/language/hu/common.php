2e64ebbc241214544c72d157d1e0ac844dfc58c1
KillBill <kill_bill@jatek-vilag.com>
2024-10-11 13:23:02 +0200

    Version: 1.0.0

---
+++ JVA-MODx_commands/ext/jv/modxcommands/language/hu/common.php
@@ -0,0 +1,49 @@
+<?php
+/**
+*
+* @package JVA - MODx commands
+* @version $Id$
+* @author 2011-2020 KillBill
+* @copyright (c) 2014-2020 https://jv-arcade.com/ - https://jv-arcade.com/contact
+* @license https://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+// Some characters you may want to copy&paste: ‚ ‘ ’ « » „ “ ” …
+
+$lang = array_merge($lang, array(
+	'JV_MODX_CMD'			=> 'MODx parancsok',
+	'JV_MODX_FIND'			=> 'Keresd',
+	'JV_MODX_AFTER'			=> 'Add utána',
+	'JV_MODX_BEFORE'		=> 'Add elé',
+	'JV_MODX_REPLACE'		=> 'Cseréld ki',
+	'JV_MODX_DELETE'		=> 'Keresd és töröld',
+	'JV_MODX_INFIND'		=> 'Keresd a sorban',
+	'JV_MODX_INAFTER'		=> 'Sorban add utána',
+	'JV_MODX_INBEFORE'		=> 'Sorban add elé',
+	'JV_MODX_INREPLACE'		=> 'Sorban cseréld ki',
+));
